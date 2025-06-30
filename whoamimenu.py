812a2f01f03a488338ffced02a45a0db57643604
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2025-06-30 21:34:59 +1000

    Last Modify
    Last Modify

--- whoamimenu.py
+++ whoamimenu.py
@@ -4,13 +4,12 @@
 
 import streamlit as st
 
-st.write("## 나는 누구인가 :red[Who am I ?] ")  
+st.write("## 나는 누구인가 :red[Who am I ?] by Kevin ")  
 st.write ("### 어디에서 와서 어디로 가는지   ", "  잠시 생각하는 시간을  "   ) 
 
-st.write("철학적인 면에서 .. ")
-st.write("과학적인 면애서 ..  ")
-st.write("종교적인 면에서 ..  ")
-st.write(" 요즘 핫한  인공지능은 뭐라 하는지  보고 들어 보시길  .. ")
+st.write("철학적, 과학적, 종교적 면에서   ")
+
+st.write("인공지능은 뭐라 하는지  보고 들어 보시길  .. ")
 
 st.markdown(
 """    
@@ -18,8 +17,7 @@ st.markdown(
     [1  Video 로 보기       ](https://youtu.be/JdrneUndDhA)\n
 
     [2  Podcast   대화혁식으로  듣기   ](https://youtu.be/kpb6-K57Wvo)\n 
-    [3  보고서 형태로 한눈에 보기   ](https://youtu.be/WWXiY0cxttI)\n
+    [3  보고서 형태로 한눈에 보기   ](https://whoami-pdf.streamlit.app/)\n
 
-     
 """
 )  
